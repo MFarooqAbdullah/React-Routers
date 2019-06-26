bbfdd21fd892da03a67e64ac09c5f9cceca799c2
Farooq <mr.farooq.ok@gmail.com>
2019-06-26 15:22:13 +0500

    -simple router page react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import Routes from "./routes"
+import { Router } from "react-router-dom"
+import createHistory from 'history/createBrowserHistory'
+import * as serviceWorker from "./serviceWorker"
+import Header from './header'
+const history = createHistory()
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router history={history}>
+        <Header />
+        <Routes />
+    </Router>,
+    document.getElementById("root")
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
